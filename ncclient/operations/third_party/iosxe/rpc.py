70ef99b32d86c1c3ba4b2be7fe871007c71cc905
Tobias Johansson <tobias.johansson@sdnit.se>
2024-04-18 10:11:15 +0200

    Update rpc.py

--- ncclient/operations/third_party/iosxe/rpc.py
+++ ncclient/operations/third_party/iosxe/rpc.py
@@ -4,6 +4,29 @@ from ncclient.xml_ import *
 from ncclient.operations.rpc import RPC
 
 class SaveConfig(RPC):
+    """Copy the running-config to startup-config on the Network Element"""
+    
     def request(self):
         node = etree.Element(qualify('save-config', "http://cisco.com/yang/cisco-ia"))
+        return self._request(node)
+
+class IsSyncing(RPC):
+    """Get the syncing status of the network element"""
+    
+    def request(self):
+        node = etree.Element(qualify('is-syncing', "http://cisco.com/yang/cisco-ia"))
+        return self._request(node)
+
+class SyncFromRunningToConfD(RPC):
+    """Synchronize the network element's running-configuration to ConfD"""
+    
+    def request(self):
+        node = etree.Element(qualify('sync-from', "http://cisco.com/yang/cisco-ia"))
+        return self._request(node)
+
+class CurrentTime(RPC):
+    """Get the current time on the network element"""
+    
+    def request(self):
+        node = etree.Element(qualify('current-time', "http://cisco.com/yang/cisco-ia"))
         return self._request(node)
