8c03aa6597eafe3b17a9feb683a171861e425a51
Tobias Johansson <tobias.johansson@sdnit.se>
2024-04-18 11:20:50 +0200

    cisco iosxe operations added for DMI self-mgmt

--- ncclient/devices/iosxe.py
+++ ncclient/devices/iosxe.py
@@ -14,37 +14,57 @@ generic information needed for interaction with a Netconf server.
 
 from .default import DefaultDeviceHandler
 
-from ncclient.operations.third_party.iosxe.rpc import SaveConfig
+from ncclient.operations.third_party.iosxe.rpc import (
+    SaveConfig,
+    SyncFromRunningToConfD,
+    CurrentTime,
+    IsSyncing,
+    Revert,
+    Rollback,
+    Checkpoint,
+    NetconfSessionId,
+)
 from ncclient.xml_ import BASE_NS_1_0
 
 import logging
+
 logger = logging.getLogger("ncclient.devices.iosxe")
 
 
 def iosxe_unknown_host_cb(host, fingerprint):
-        # This will ignore the unknown host check when connecting to CSR devices
-        return True
+    # This will ignore the unknown host check when connecting to CSR devices
+    return True
+
 
 class IosxeDeviceHandler(DefaultDeviceHandler):
     """
     Cisco IOS-XE handler for device specific information.
-
     """
+
     def __init__(self, device_params):
         super(IosxeDeviceHandler, self).__init__(device_params)
 
     def add_additional_operations(self):
-        dict = {}
-        dict["save_config"] = SaveConfig
-        return dict
-        
+        """Return all the additional operations"""
+
+        return {
+            "save_config": SaveConfig,
+            "sync_from": SyncFromRunningToConfD,
+            "current_time": CurrentTime,
+            "is_syncing": IsSyncing,
+            "rollback": Rollback,
+            "checkpoint": Checkpoint,
+            "revert": Revert,
+            "netconf_session_id": NetconfSessionId,
+        }
+
     def add_additional_ssh_connect_params(self, kwargs):
-        kwargs['unknown_host_cb'] = iosxe_unknown_host_cb
+        kwargs["unknown_host_cb"] = iosxe_unknown_host_cb
 
     def transform_edit_config(self, node):
         # find the first node that has the tag "config" with no namespace
         nodes = node.findall("./config")
         if len(nodes) == 1:
-            logger.debug('IOS XE handler: patching namespace of config element')
-            nodes[0].tag = '{%s}%s' % (BASE_NS_1_0, 'config')
+            logger.debug("IOS XE handler: patching namespace of config element")
+            nodes[0].tag = "{%s}%s" % (BASE_NS_1_0, "config")
         return node
--- ncclient/operations/third_party/iosxe/rpc.py
+++ ncclient/operations/third_party/iosxe/rpc.py
@@ -3,30 +3,68 @@ from lxml import etree
 from ncclient.xml_ import *
 from ncclient.operations.rpc import RPC
 
+
 class SaveConfig(RPC):
     """Copy the running-config to startup-config on the Network Element"""
-    
+
     def request(self):
-        node = etree.Element(qualify('save-config', "http://cisco.com/yang/cisco-ia"))
+        node = etree.Element(qualify("save-config", "http://cisco.com/yang/cisco-ia"))
         return self._request(node)
+
 
 class IsSyncing(RPC):
     """Get the syncing status of the network element"""
-    
+
     def request(self):
-        node = etree.Element(qualify('is-syncing', "http://cisco.com/yang/cisco-ia"))
+        node = etree.Element(qualify("is-syncing", "http://cisco.com/yang/cisco-ia"))
         return self._request(node)
 
-class SyncFromRunningToConfD(RPC):
-    """Synchronize the network element's running-configuration to ConfD"""
-    
-    def request(self):
-        node = etree.Element(qualify('sync-from', "http://cisco.com/yang/cisco-ia"))
-        return self._request(node)
 
 class CurrentTime(RPC):
     """Get the current time on the network element"""
-    
+
     def request(self):
-        node = etree.Element(qualify('current-time', "http://cisco.com/yang/cisco-ia"))
+        node = etree.Element(qualify("current-time", "http://cisco.com/yang/cisco-ia"))
+        return self._request(node)
+
+
+class NetconfSessionId(RPC):
+    """get the network element's assigned netconf session id"""
+
+    def request(self):
+        node = etree.Element(
+            qualify("netconf-session-id", "http://cisco.com/yang/cisco-ia")
+        )
+        return self._request(node)
+
+
+class Checkpoint(RPC):
+    """Create a configuration rollback checkpoint"""
+
+    def request(self):
+        node = etree.Element(qualify("checkpoint", "http://cisco.com/yang/cisco-ia"))
+        return self._request(node)
+
+
+class SyncFromRunningToConfD(RPC):
+    """Synchronize the network element's running-configuration to ConfD"""
+
+    def request(self, input: dict = {}):
+        node = etree.Element(qualify("sync-from", "http://cisco.com/yang/cisco-ia"))
+        return self._request(node)
+
+
+class Revert(RPC):
+    """Cancel the timed rollback and trigger rollback, or modify the timed rollback"""
+
+    def request(self, input: dict = {}):
+        node = etree.Element(qualify("revert", "http://cisco.com/yang/cisco-ia"))
+        return self._request(node)
+
+
+class Rollback(RPC):
+    """Cancel the timed rollback and trigger rollback, or modify the timed rollback"""
+
+    def request(self, input: dict = {}):
+        node = etree.Element(qualify("rollback", "http://cisco.com/yang/cisco-ia"))
         return self._request(node)
